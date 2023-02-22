c73875dc5c23eaf4c5198fe3857c9b5dd619122b
Miraaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <kadyrbaym@inbox.ru>
2023-02-22 15:19:49 +0600

    initial

---
+++ my_project2/slider.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 1000
+  });
